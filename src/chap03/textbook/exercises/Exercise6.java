3eb541cf086e7c34e6951d029f42827219e8a330
pedroyoung95 <jty9531@gmail.com>
2020-10-06 09:43:35 +0900

    10/6 9:43 수업내용

--- src/chap03/textbook/exercises/Exercise6.java
+++ src/chap03/textbook/exercises/Exercise6.java
@@ -6,6 +6,8 @@ public class Exercise6 {
 		int lengthBottom = 10;
 		int height = 7;
 		double area = (double)((lengthTop+lengthBottom) * height)/ 2;
+//		윗변, 아랫변, 높이 전부 int이므로 산술값도 연산
+//		소수점 계산을 위해 2.0으로 나누거나, 값을 double로 변환해야 함
 		System.out.println(area);
 	}
 }
