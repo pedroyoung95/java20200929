b6df66974ca05bfe3c10cd5849f7e9d3a91b62d1
pedroyoung95 <jty9531@gmail.com>
2020-10-07 17:50:00 +0900

    10/7 17:49 수업내용

---
+++ src/chap04/exercises/LoopPractice.java
@@ -0,0 +1,96 @@
+package chap04.exercises;
+
+public class LoopPractice {
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("---------------");
+		
+		for(int i=5; i>0; i--) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("---------------");
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			for(int k=5; k>i; k--) {
+				System.out.print("#");
+			}
+			System.out.println();							
+		}
+		System.out.println("---------------");
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("#");
+			}
+			for(int k=5; k>i; k--) {
+				System.out.print("*");
+			}
+			System.out.println();							
+		}
+		System.out.println("---------------");
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=5; k>i; k--) {
+				System.out.print("*");
+			}
+			System.out.println();							
+		}
+		System.out.println("---------------");
+		
+		for(int i=0; i<5; i++) {
+			for(int k=5; k>i; k--) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();							
+		}
+		System.out.println("---------------");
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		System.out.println("---------------");
+		
+		for(int i=0; i<5; i++) {
+			for(int j=i; j<i+5; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		System.out.println("---------------");
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		System.out.println("---------------");
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++) {
+				
+			}
+			System.out.println();
+		}
+	}
+}
