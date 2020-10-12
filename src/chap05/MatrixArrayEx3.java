311e11b249872ee441fa974636a123fd536197de
pedroyoung95 <jty9531@gmail.com>
2020-10-12 10:37:33 +0900

    10/12 10:37 수업내용

---
+++ src/chap05/MatrixArrayEx3.java
@@ -0,0 +1,11 @@
+package chap05;
+
+public class MatrixArrayEx3 {
+	public static void main(String[] args) {
+		int[] arr = {4, 5, 6};
+		
+		int[][] mat1 = { {1, 2}, {99, 88}, {7, 8} };
+//배열 안 배열의 값을 직접 넣어서 중복배열 생성 가능
+		int[][] mat2 = { {2, 3}, {9, 8, 7} };
+	}
+}
