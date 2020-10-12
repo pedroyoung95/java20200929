23febdb1ce8d1e957735c27d65bb64e02a95b223
pedroyoung95 <jty9531@gmail.com>
2020-10-12 23:06:58 +0900

    10/12 369 game

---
+++ src/myProject/game/ThreeSixNineGame.java
@@ -0,0 +1,19 @@
+package myProject.game;
+
+import java.util.Scanner;
+
+public class ThreeSixNineGame {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean run = true;
+		int num = 1;
+		
+		while(run) {
+			
+		}
+		
+	}
+
+}
