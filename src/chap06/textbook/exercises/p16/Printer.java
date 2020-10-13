105f57ba45eba473d24d1ded0ca62c94e2de1e72
pedroyoung95 <jty9531@gmail.com>
2020-10-13 14:46:29 +0900

    10/13 14:46 수업내용

--- src/chap06/textbook/exercises/p16/Printer.java
+++ src/chap06/textbook/exercises/p16/Printer.java
@@ -1,28 +1,19 @@
 package chap06.textbook.exercises.p16;
 
 public class Printer {
-	int i;
-	boolean True;
-	double Double;
-	String name;
-	
 	void println(int i) {
-		this.i = i;		
 		System.out.println(i);
 	}
 	
 	void println(boolean True) {
-		this.True = True;
 		System.out.println(True);
 	}
 	
 	void println(double Double) {
-		this.Double = Double;
 		System.out.println(Double);
 	}
 	
 	void println(String name) {
-		this.name = name;
 		System.out.println(name);
 	}
 }
