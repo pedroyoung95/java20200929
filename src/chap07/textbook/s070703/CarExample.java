b26d0f21923047cbbbf70014f9f61e64a1338c02
pedroyoung95 <jty9531@gmail.com>
2020-10-15 15:32:51 +0900

    10/15 15:32 수업내용

--- src/chap07/textbook/s070703/CarExample.java
+++ src/chap07/textbook/s070703/CarExample.java
@@ -1,6 +1,11 @@
 package chap07.textbook.s070703;
 
 import chap07.textbook.s070702.HankookTire;
+//HankookTire클래스의 생성자를 사용하면 super를 통해 Tire클래스의 생성자 호출
+//하지만 Tire를 import해도 HankookTire는 자식클래스이기 때문에 
+//HankookTire의 생성자에는 접근불가
+//따라서, HankookTire를 import해서 Tire생성자까지 사용하거나
+//*로 바꿔서 모든 클래스를 사용해야 함
 
 public class CarExample {
 	public static void main(String[] args) {
@@ -10,7 +15,7 @@ public class CarExample {
 			int problemLocation = car.run();
 			if(problemLocation != 0) {
 				System.out.println(car.tires[problemLocation-1].location + " HankookTire로 교체");
-				car.tires[problemLocation-1] = new HankookTire(car.tires[problemLocation-1].location, 15);
+				car.tires[problemLocation-1] = new HankookTire(car.tires[problemLocation-1].location, 15);				
 			}
 			System.out.println("-------------------------");
 		}
