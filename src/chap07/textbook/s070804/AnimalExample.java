a1f4f895e771b90049c729618f121a8743eda4a7
pedroyoung95 <jty9531@gmail.com>
2020-10-16 12:01:02 +0900

    10/16 12:00 수업내용

--- src/chap07/textbook/s070804/AnimalExample.java
+++ src/chap07/textbook/s070804/AnimalExample.java
@@ -7,6 +7,7 @@ public class AnimalExample {
 		dog.sound();
 		cat.sound();
 		System.out.println("-----");
+//클래스에서 직접 인스턴스를 만들어서 각각의 메소드 실행
 		
 		
 		//변수의 자동타입변환
@@ -16,10 +17,12 @@ public class AnimalExample {
 		animal = new Cat();
 		animal.sound();
 		System.out.println("-----");
+//부모타입으로 자동형변환 한 후 메소드 실행(인스턴스의 메소드 실행)
 		
 		//메소드의 다형성
 		animalSound(new Dog());
 		animalSound(new Cat());		
+//인스턴스의 메소드를 실행시키는 메소드에 인스턴스를 parameter로 넣어서 실행
 	}
 	
 	public static void animalSound(Animal animal) {
