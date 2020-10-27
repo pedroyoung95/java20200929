239d3fd72c8e62f6cb471509fb43f2d84498a173
pedroyoung95 <jty9531@gmail.com>
2020-10-27 12:41:37 +0900

    10/27 12:41 수업내용

--- src/chap18/textbook/s180405/FileCopy.java
+++ src/chap18/textbook/s180405/FileCopy.java
@@ -11,8 +11,8 @@ public class FileCopy {
 		String src = "src/chap18/textbook/s180405/FileCopy.java";
 		String des = "file-copy.txt";
 		
-		Reader reader = new FileReader(src);
-		Writer writer = new FileWriter(des, true);
+		Reader reader = new FileReader(src); //파일을 읽음(그 파일이 존재해야 함)
+		Writer writer = new FileWriter(des, true); //파일을 출력(생성)
 
 //방법 1. 문자 한 개씩 읽은 걸 int 변수에 담고, 그 int변수를 출력
 //		int rc;		
