730de7b74af6dd01017e1dd1efd70abc04ab8a7e
pedroyoung95 <jty9531@gmail.com>
2020-10-30 15:44:40 +0900

    10/30 15:44 수업내용

---
+++ src/chap14/lecture/api/methodref/MethodRefEx1.java
@@ -0,0 +1,34 @@
+package chap14.lecture.api.methodref;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+class Car{
+	
+}
+
+public class MethodRefEx1 {
+	public static void main(String[] args) {
+
+//메소드 참조 가능 기준
+//호출하는 메소드의 리턴타입이 추상메소드의 리턴타입과 동일하면 메소드 참조로 축약 가능
+		
+		
+		//정적메소드 참조하는 경우
+		Supplier<Double> random = () -> Math.random();
+		Supplier<Double> ran2 = Math::random;
+
+		
+		//인스턴스메소드 참조하는 경우
+		Random rand = new Random();
+		System.out.println(rand.nextDouble());
+		System.out.println(rand.nextInt());		
+		Supplier<Integer> nextInt = () -> rand.nextInt();
+		Supplier<Integer> nextInt2 = rand::nextInt;
+		
+		//생성자 참조하는 경우
+		Supplier<Car> nextCar = () -> new Car();
+		Supplier<Car> nextCar2 = Car::new;
+		
+	}
+}
