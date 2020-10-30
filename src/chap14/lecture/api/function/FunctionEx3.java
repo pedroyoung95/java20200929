f0e0996c5a3f2f78bf6d3a72af8ac09b38e9785b
pedroyoung95 <jty9531@gmail.com>
2020-10-30 12:37:03 +0900

    10/30 12:36 수업내용

--- src/chap14/lecture/api/function/FunctionEx3.java
+++ src/chap14/lecture/api/function/FunctionEx3.java
@@ -1,15 +1,32 @@
 package chap14.lecture.api.function;
 
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FunctionEx3 {
-	public static void main(String[] args) {
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		map.compute(key, remappingFunction);
+	public static void main(String[] args) throws Exception{
+		String path = "src/chap14/lecture/api/function/FunctionEx3.java";
+		FileReader fr = new FileReader(path);
+		
+		Map<Character, Integer> map = new HashMap<>();
+		
+		int ch;
+		
+		while((ch = fr.read()) != -1) {
+			if(('a' <= ch && ch <='z') || ('A' <= ch && ch <= 'Z')) {
+				char c = (char)ch;
+				System.out.println(c);				
+				map.compute(c, (k, v) -> v == null ? 1 : v + 1);	
 //compute(): 첫 번째 파라미터는 key
 //두 번째 파라미터는 BiFunction 인터페이스
 //BiFunction인터페이스의 람다식의 결과값을 key의 새로운 값으로 할당하게 됨
 //map.put(k, v)에서 새로 넣을 값이 람다식인 형태
+			}			
+		}
+		
+		map.forEach((k, v) -> System.out.println(k + ":" + v));
+		
+		fr.close();
 	}
 }
